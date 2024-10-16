15771fee9a0074dacb7a941751e2458eb65d8a1b
MaRt <ogbemart@gmail.com>
2024-10-16 09:55:51 +0100

    furniture haven 2

--- script.js
+++ script.js
@@ -2,5 +2,3 @@ const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
 const navLinks = document.querySelectorAll(".nav_link");
 
-
-menu.addEventListener("click", () => nav.classList.toggle("close"));
